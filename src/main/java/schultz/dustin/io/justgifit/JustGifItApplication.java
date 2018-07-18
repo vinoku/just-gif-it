59dbf709cb28fd09de98ed5cbc47f1476ab5b476
Vinh Nguyen <vinoku78@gmail.com>
2018-07-18 21:46:06 +0200

    add upload controller

---
+++ src/main/java/schultz/dustin/io/justgifit/JustGifItApplication.java
@@ -0,0 +1,12 @@
+package schultz.dustin.io.justgifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGifItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGifItApplication.class, args);
+	}
+}
---
+++ src/main/java/schultz/dustin/io/justgifit/controller/UploadController.java
@@ -0,0 +1,36 @@
+package schultz.dustin.io.justgifit.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class UploadController {
+
+	private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	@Value("${spring.http.multipart.location}")
+	private String location;
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.IMAGE_GIF_VALUE)
+	public String upload(@RequestPart("file") MultipartFile file, @RequestParam("start") int start,
+			@RequestParam("end") int end, @RequestParam("speed") int speed, @RequestParam("repeat") boolean repeat)
+			throws IllegalStateException, IOException {
+		File videoFile = new File(location + "/" + System.currentTimeMillis() + ".mp4");
+		file.transferTo(videoFile);
+
+		log.info("Saved file to {}", videoFile.getAbsolutePath());
+		return "";
+	}
+}
